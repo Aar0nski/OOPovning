b10aa4e24fe9dd957ee930c452df94d866472c8e
Aar0nski <aaron.enander04@gmail.com>
2025-09-18 13:54:39 +0200

    La till en main metod samt virtual och override metod

--- ConsoleApp1/BankAccount.cs
+++ ConsoleApp1/BankAccount.cs
@@ -7,15 +7,47 @@ using System.Threading.Tasks;
 namespace ConsoleApp1
 {
     // Skapar en klass 
-    public class BankAccount 
+    public class BankAccount
     {
         // Egenskapaper för kontonummer och saldo
-        public string AccountNumber { get; set; } 
-        public double Balance { get; set; }
+        public string AccountNumber { get; set; }
+        // Jag gör balance privat så vi låser den från direkt åtkomst
+        private double Balance;
+
+        // Kontsruktor som körs när ett nytt konto skapas, AccountNumber och InitialBalance sätter konto nummer och startbelopp.
+
+        public BankAccount(string accountNumber, double initialBalance)
+        {
+            AccountNumber = accountNumber;
+            // Jag sätter initial balance till 0 om det är negativt. Det gör så att ett konto aldrig kan starta på minus. Så om Initial Balance är -100 så blir det 0 istället.
+            Balance = Math.Max(0, initialBalance);
+        }
+        // En metod för att sätta in pengar på kontot, Math .Max(0, amount) gör så att man inte kan sätta in negativa belopp.
+        public void Deposit(double amount)
+        {
+            Balance += Math.Max(0, amount);
+        }
+        // En metod för att ta ut pengar från kontot, kollar så att det finns tillräckligt med pengar på kontot innan uttag.
+        public void Whitdraw(double amount)
+        {
+            if (Balance - amount >= 0)
+            {
+                Balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Otillräckligt saldo.");
+            }
+
+        }
         // En metod som skriver ut kontoinformation 
-        public void PrintAccountInfo() 
+        // virtual gör så att metoden kan overrideas i en subklass
+        public virtual void PrintAccountInfo()
         {
             Console.WriteLine($"Account: {AccountNumber}, Balance: {Balance}");
-        } 
-    } 
+        }
+        // En metod som returnerar saldot.
+        // Har gjort den protected så att den kan användas i subklasser
+        protected double GetBalance() => Balance;
+    }
 }
---
+++ ConsoleApp1/SavingAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class SavingsAccount : BankAccount // Arv från BankAccount
+    {
+        public double InterestRate { get; set; } // Ny prop som bara finns i SavingsAccount eftersom att ett bankkonto inte har ränta.
+
+        public SavingsAccount(string accountNumber, double initialBalance, double interestRate) // det som körs när man skapar nytt sparkonto
+            : base(accountNumber, initialBalance) // skickar vidare accountNumber och initialBalance till basklassens konstruktor
+        {
+            InterestRate = interestRate; // sparar räntan
+        }
+        public override void PrintAccountInfo() // overridear basklassens PrintAccountInfo för att lägga till räntan i utskriften
+        {
+            Console.WriteLine(
+                $"Account: {AccountNumber}, Balance: {GetBalance()}, Interest Rate: {InterestRate * 100}%"
+                ); 
+        }
+
+    }
+}
