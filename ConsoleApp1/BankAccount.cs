bb9bcd073f9301b609f43bf2a45272dba19df36b
Aar0nski <aaron.enander04@gmail.com>
2025-09-18 11:12:18 +0200

    La till en bankaccount klass med två egenskaper och en metod

---
+++ ConsoleApp1/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    // Skapar en klass 
+    public class BankAccount 
+    {
+        // Egenskapaper för kontonummer och saldo
+        public string AccountNumber { get; set; } 
+        public double Balance { get; set; }
+        // En metod som skriver ut kontoinformation 
+        public void PrintAccountInfo() 
+        {
+            Console.WriteLine($"Account: {AccountNumber}, Balance: {Balance}");
+        } 
+    } 
+}
